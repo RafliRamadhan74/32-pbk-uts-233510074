ec16b72c96240975bd16c68c4d9587e83d264354
RafliRamadhan74 <rafliramadhan@student.uir.ac.id>
2025-05-02 11:08:01 +0700

    Menampilkan tugas

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,12 @@ const addTask = () => {
 <template>
  <input type="text" name="" v-model="newTask" placeholder="Add a new task" @keyup.enter="addTask">
  <button @click="addTask">add task</button>
+
+ <ul>
+  <li v-for="task in tasks" :key="task.id">
+    {{ task.text }}
+  </li>
+ </ul>
 </template>
 
 <style scoped>
