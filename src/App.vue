87da8067d733cf591ee21a5cc9bd7421a2dfda16
RafliRamadhan74 <rafliramadhan@student.uir.ac.id>
2025-05-02 11:27:54 +0700

    filter tugas

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const tasks = ref([])
 const newTask = ref('')
+const filter = ref('all')
 
 const addTask = () => {
   if (newTask.value.trim() !== '') {
@@ -25,14 +26,30 @@ const removeTask = (task) => {
   tasks.value = tasks.value.filter(t => t.id !== task.id)
 }
 
+const filteredTask = computed(() =>{
+  if(filter.value === "completed"){
+    return tasks.value.filter(t => t.completed)
+  }else if (filter.value === "incompleted"){
+    return tasks.value.filter(t => !t.completed)
+  }else {
+    return tasks.value
+  }
+})
+
 </script>
 
 <template>
   <input type="text" name="" v-model="newTask" placeholder="Add a new task" @keyup.enter="addTask">
   <button @click="addTask">add task</button>
 
+  <select v-model="filter">
+    <option value="all">All</option>
+    <option value="completed">Completed</option>
+    <option value="incompleted">incompleted</option>
+  </select>
+
   <ul>
-    <li v-for="task in tasks" :key="task.id">
+    <li v-for="task in filteredTask" :key="task.id">
       <input type="checkbox" v-model="task.completed" @change="toggleTask(task)" :checked="task.completed">
       {{ task.text }}
       <button @click="removeTask(task)">Remove</button>
