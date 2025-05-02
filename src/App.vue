b4b39a0bcbd06bc6d86ddacc0ec5e92e8a636a45
RafliRamadhan74 <rafliramadhan@student.uir.ac.id>
2025-05-02 10:57:27 +0700

    Menambah data tugas

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+const tasks = ref([])
+const newTask = ref('')
+
+const addTask = () => {
+  if (newTask.value.trim() !== '') {
+    tasks.value.push({
+      id: Date.now(),
+      text: newTask.value,
+      completed: false
+    })
+    newTask.value = ''
+    console.log(tasks.value)
+  }}
+
+</script>
+
+<template>
+ <input type="text" name="" v-model="newTask" placeholder="Add a new task" @keyup.enter="addTask">
+ <button @click="addTask">add task</button>
+</template>
+
+<style scoped>
+
+</style>
